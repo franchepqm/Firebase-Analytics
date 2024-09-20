d2730a5535a51bcb05edd01327a2214fca103b04
franchepqm_ <74469603+franchepqm@users.noreply.github.com>
2024-09-20 16:52:51 -0300

    Create analytics.js

---
+++ analytics.js
@@ -0,0 +1,5 @@
+    import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js'
+    const firebaseConfig = {
+      // ...
+    };
+    const analytics = getAnalytics(app);
